e2bb6bece0b4d6d15ffca73c611cfaa5ca24c48a
Ilya <senior.smikow@yandex.ru>
2021-12-19 21:06:03 +0300

    Сделал д/з про раскрывающийся список

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { App } from './tutorial_5/App';
+import { App } from './tutorial_6/App';
 
 
 ReactDOM.render(
